47e0b0ea8a115f40ea4ffef4293af0f8dc7de8d2
Ameya Jathar <ameyaj20010@gmail.com>
2017-06-09 08:40:31 +0530

    Adding Algo JSON and parsing code.

---
+++ Algorithms/AlgorithmFactory.swift
@@ -0,0 +1,13 @@
+//
+//  AlgorithmFactory.swift
+//  Algorithms
+//
+//  Created by Ameya Jathar on 09/06/17.
+//  Copyright © 2017 Ngyn. All rights reserved.
+//
+
+import UIKit
+
+class AlgorithmFactory: NSObject {
+
+}
--- Algorithms/ViewController.swift
+++ Algorithms/ViewController.swift
@@ -8,18 +8,64 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var algoTable: UITableView!
+    let algoCellIdentifier = "ALgoCell"
+    var algoArray: Array<Dictionary<String, String>> = []
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        algoTable.register(UITableViewCell.self, forCellReuseIdentifier: algoCellIdentifier)
+        algoArray = getAlgoJSON()
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    //Tableview dataSource methods
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1;
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return algoArray.count;
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell:UITableViewCell = tableView.dequeueReusableCell(withIdentifier: algoCellIdentifier)!
+        
+        cell.textLabel?.text = algoArray[indexPath.row]["Algo"]
+        return cell;
+    }
+    
+    
+    //Read Algorithms JSON
+    private func getAlgoJSON() -> Array<Dictionary<String, String>> {
+        do {
+            if let file = Bundle.main.url(forResource: "AlgoList", withExtension: "json") {
+                let data = try Data(contentsOf: file)
+                let json = try JSONSerialization.jsonObject(with: data, options: [])
+                if let object = json as? [String: Any] {
+                    return object["Algorithms"] as! Array<Dictionary<String, String>>
+                } else if let object = json as? [Any] {
+                    // json is an array
+                    print(object)
+                } else {
+                    print("JSON is invalid")
+                }
+            } else {
+                print("no file")
+            }
+        } catch {
+            print(error.localizedDescription)
+        }
+        
+        return []
+    }
 }
-
